e448674b39091a553a1c69122e73c0541e918306
anishzute <anishzute@utexas.edu>
2020-05-12 19:34:52 -0500

    added checking to see if a new auth or refresh  token is needed. Also added sketch for future of project.

--- OptionsProfitCalc.py
+++ OptionsProfitCalc.py
@@ -3,14 +3,37 @@ from tdameritrade import auth
 
 client_id = os.getenv('TDAMERITRADE_CLIENT_ID')
 account_id = os.getenv('TDAMERITRADE_ACCOUNT_ID')
-#refresh_token = os.getenv('TDAMERITRADE_REFRESH_TOKEN')
 redirect_uri = os.getenv('REDIRECT_URL')
+# access_token = os.getenv('ACCESS_TOKEN')
+# refresh_token = os.getenv('TDAMERITRADE_REFRESH_TOKEN')
 #print(redirect_uri+client_id)
 #tdameritrade.auth.authentication(client_id,redirect_uri,os.getenv('TDAUSER'), os.getenv('TDAPASS'))
-auth = tdameritrade.auth.authentication(client_id,redirect_uri,os.getenv('TDAUSER'), os.getenv('TDAPASS'))
+#print(os.getenv['ACCESS_TOKEN'])
 
-os.environ['TDAMERITRADE_REFRESH_TOKEN'] = auth['refresh_token']
-os.environ['ACCESS_TOKEN'] = auth['access_token']
-td = tdameritrade.TDClient()
+def getTokens():
+    auth = tdameritrade.auth.authentication(client_id, redirect_uri, os.getenv('TDAUSER'), os.getenv('TDAPASS'))
+    os.environ['TDAMERITRADE_REFRESH_TOKEN'] = auth['refresh_token']
+    os.environ['ACCESS_TOKEN'] = auth['access_token']
 
-print(td.quote('SPY'))
+
+try:
+    td = tdameritrade.TDClient()
+    td.quote('SPY')
+except tdameritrade.exceptions.InvalidAuthToken:
+    print("Token Expire: Getting new token.")
+    getTokens()
+    td = tdameritrade.TDClient()
+except KeyError:
+    print('Keyerror passed')
+    getTokens()
+    td = tdameritrade.TDClient()
+
+
+
+
+
+#print(auth['refresh_token'])
+
+
+print(td.optionsDF('JCP'))
+
